15cae4ad3f6dcee450a2acc95cfafa42b3fe7e4c
leepuppychow <leepuppychow@gmail.com>
2019-03-03 15:00:28 -0700

    Adding routes file

---
+++ routes/routes.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/leepuppychow/employees_API/controllers"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Url         string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	{"PapersIndex", "GET", "/employees", controllers.EmployeesIndex},
+}
+
+func NewRouter(useAuth bool) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Url).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+	return router
+}
